fbab5244066c429f2aeaaa0dca48743daeb4a5d9
Yan Savitski <yan.savitski@elastic.co>
2023-06-05 18:26:53 +0200

    Remove template file

--- src/query_builder.ts
+++ src/query_builder.ts
@@ -9,17 +9,17 @@ export interface IQueryBuilder {
   addFilter: (field: string, value: Filter) => IQueryBuilder
   addFacetFilter: (field: string, value: Filter) => IQueryBuilder
   addParameter: (parameter: string, value: string) => IQueryBuilder
-
   query: (query: string) => IQueryBuilder
-
   search: () => Promise<any>
 }
 
 export class QueryBuilder implements IQueryBuilder {
   private params = { params: { query: '' } }
+
   constructor(private readonly apiClient: IAPI, baseParams) {
     this.params.params = baseParams
   }
+
   addFacetFilter(field, value): IQueryBuilder {
     return this
   }
--- src/template.ts
+++
@@ -1,9 +0,0 @@
-export interface TestTemplateParams {
-    facets: Record<string, { size: number }>
-    search_fields: string[]
-    result_fields
-}
-
-export const testTemplate = (params: TestTemplateParams) => {
-    return {}
-}
