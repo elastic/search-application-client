b6f4a1639c95d8d560aba010dd8bcf012e3f17db
Yan Savitski <yan.savitski@elastic.co>
2023-06-08 13:15:43 +0200

    Add facet filter to query builder

--- src/__test__/query_builder.test.ts
+++ src/__test__/query_builder.test.ts
@@ -79,4 +79,63 @@ describe('QueryBuilder', () => {
       })
     })
   })
+
+  describe('addFacetFilter', () => {
+    test('should throw error when facet not passed in configuration params', () => {
+      expect(() => {
+        queryBuilder.addFacetFilter('Rated', 'PG')
+      }).toThrow(`Facet Rated wasn't passed in configuration params`)
+    })
+
+    test('should update _es_filters when proper param passed to addFacetFilter', () => {
+      const queryBuilderWithFacet = new QueryBuilder(new API('', '', ''), {
+        facets: {
+          actors: {
+            field: 'actors.enum',
+            size: 10,
+            type: 'terms',
+          },
+          directors: {
+            field: 'directors.enum',
+            size: 10,
+            type: 'terms',
+            disjunctive: true,
+          },
+          imdbrating: {
+            field: 'imdbrating',
+            type: 'stats',
+          },
+        },
+      })
+
+      queryBuilderWithFacet
+        .addFacetFilter('actors', 'PG')
+        .addFacetFilter('directors', ['Steven Spielberg', 'Robert Zemeckis'])
+        .addFacetFilter('imdbrating', { gte: 7.5 })
+
+      expect(queryBuilderWithFacet.params.params._es_filters).toEqual({
+        bool: {
+          must: [
+            {
+              terms: {
+                'actors.enum': 'PG',
+              },
+            },
+            {
+              terms: {
+                'directors.enum': ['Steven Spielberg', 'Robert Zemeckis'],
+              },
+            },
+            {
+              range: {
+                imdbrating: {
+                  gte: 7.5,
+                },
+              },
+            },
+          ],
+        },
+      })
+    })
+  })
 })
--- src/query_builder.ts
+++ src/query_builder.ts
@@ -1,21 +1,39 @@
 import { API } from './api'
 
-type FilterValue =
-  | string
-  | string[]
-  | { from: number; to: number }
-  | { from: number; to: number }[]
-  | number
-  | number[]
+type BaseValueFilter = string | string[] | number | number[]
+type BaseRangeFilter = {
+  gte?: number
+  lte?: number
+  gt?: number
+  lt?: number
+}
+type FilterFieldValue = BaseValueFilter | BaseRangeFilter
+type FilterMatchField = { match: { [field: string]: BaseValueFilter } }
+type FilterTermsField = { terms: { [field: string]: BaseValueFilter } }
+type FilterRangeField = { range: { [field: string]: BaseRangeFilter } }
 
-type Filter = Record<
-  'match' | 'terms' | 'range',
-  { [field: string]: FilterValue }
->
+type FilterField = FilterMatchField | FilterRangeField | FilterTermsField
+
+enum FacetType {
+  terms = 'terms',
+  stats = 'stats',
+}
+
+interface FacetParam {
+  field: string
+  size?: number
+  type: FacetType
+  disjunctive?: boolean
+}
+
+const mapFacetTypeToElastic: { [key: string]: 'terms' | 'range' } = {
+  [FacetType.terms]: 'terms',
+  [FacetType.stats]: 'range',
+}
 
 interface Params {
   params: {
-    _es_filters?: { bool: { must: Filter[] } }
+    _es_filters?: { bool: { must: FilterField[] } }
     query?: string
     result_fields?: string[]
     search_fields?: string[]
@@ -25,21 +43,34 @@ interface Params {
 }
 
 export class QueryBuilder {
-  private params: Params = { params: {} }
+  private readonly facets: Record<string, FacetParam>
+  params: Params = { params: {} }
 
   constructor(private readonly apiClient: API, baseParams) {
-    this.params.params = baseParams
+    this.facets = baseParams.facets
   }
 
-  addFacetFilter(field: string, value: FilterValue): this {
+  addFacetFilter(field: string, value: FilterFieldValue): this {
+    const facetInfo = this.facets?.[field]
+
+    if (!facetInfo) {
+      throw new Error(`Facet ${field} wasn't passed in configuration params`)
+    }
+
+    const elasticType = mapFacetTypeToElastic[facetInfo.type]
+
+    this.setFilter({
+      [elasticType]: { [facetInfo.field]: value },
+    } as FilterField)
+
     return this
   }
 
-  addFilter(field: string, value: FilterValue): this {
+  addFilter(field: string, value: FilterFieldValue): this {
     return this
   }
 
-  setFilter(value: Filter): this {
+  setFilter(value: FilterField): this {
     if (!this.params.params._es_filters) {
       this.params.params._es_filters = { bool: { must: [] } }
     }
