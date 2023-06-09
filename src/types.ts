e1985768b464b16bf23078c29bc84009914c4741
Joseph McElroy <joseph.mcelroy@elastic.co>
2023-06-09 16:04:22 +0100

    improve filter types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,74 @@
+export type BaseValueFilter = string | string[] | number | number[]
+export type BaseRangeFilter = {
+  gte?: number
+  lte?: number
+  gt?: number
+  lt?: number
+}
+export type FilterFieldValue = BaseValueFilter | BaseRangeFilter
+
+export type Params = Record<string, unknown>
+
+export type FacetFilters = Record<string, FilterFieldValue[]>
+
 export type SortFields = (Record<string, 'desc' | 'asc'> | '_score')[]
+
+export type BoolQuery = {
+  must?: Query[]
+  filter?: Query[]
+  should?: Query[]
+  must_not?: Query[]
+}
+
+export type BaseQueryValue = string | number | boolean
+
+export type MatchQuery = {
+  [field: string]: {
+    query: BaseQueryValue
+  }
+}
+
+export type TermQuery = {
+  [field: string]: BaseQueryValue
+}
+
+export type RangeQuery = {
+  [field: string]: BaseRangeFilter
+}
+
+export type NestedQuery = {
+  path: string
+  query: Query
+}
+
+export type Query = {
+  match?: MatchQuery
+  term?: TermQuery
+  range?: RangeQuery
+  nested?: NestedQuery
+  geo_bounding_box?: GeoBoundingBoxQuery
+  geo_distance?: GeoDistanceQuery
+}
+
+export type GeoBoundingBoxQuery = {
+  [field: string]: {
+    top_left: {
+      lat: number
+      lon: number
+    }
+    bottom_right: {
+      lat: number
+      lon: number
+    }
+  }
+}
+
+export type GeoDistanceQuery = {
+  distance: string
+} & Record<
+  string,
+  {
+    lat: number
+    lon: number
+  }
+>
