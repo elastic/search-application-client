61739f8565d87f151719d4cff85d1a490d653e32
Tim Grein <tim.grein@elastic.co>
2023-10-25 15:38:41 +0200

    Add tests for the SearchApplicationClient

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,7 +1,19 @@
-import Client from '../index'
+import SearchApplicationClient from '../index'
 
-describe('Client', () => {
-  test('should be defined', () => {
-    expect(Client).toBeDefined()
-  })
+describe('SearchApplicationClient', () => {
+    test('should throw error on missing application name', () => {
+        expect(() => SearchApplicationClient("", "", "")).toThrow(Error("applicationName is required"));
+    })
+
+    test('should throw error on missing endpoint', () => {
+        expect(() => SearchApplicationClient("application name", "", "")).toThrow(Error("endpoint is required"));
+    })
+
+    test('should throw error on missing apiKey', () => {
+        expect(() => SearchApplicationClient("application name", "endpoint", "")).toThrow(Error("apiKey is required"));
+    })
+
+    test('should not throw error on missing params', () => {
+        expect(() => SearchApplicationClient("application name", "endpoint", "api key")).not.toThrow();
+    })
 })
